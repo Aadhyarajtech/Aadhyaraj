f068aa956ff3f7f2125479cd71a17c242da45511
Sreeshanth Reddy <dsreeshanth48@gmail.com>
2026-06-04 17:35:05 +0530

    nodemailer1

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,5 +1,8 @@
 const nodemailer = require('nodemailer');
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
@@ -7,6 +10,14 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  }
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Mailer verify error:", error);
+  } else {
+    console.log("Mailer ready");
   }
 });
 
